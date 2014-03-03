f41826cec863a7391a440a164e54f62a97203a68
Albert Lombarte <alombarte@gmail.com>
2014-03-03 19:55:49 +0100

    Allow the script to be executed several times

--- src/sphinx/2-mysql_demo_data.sh
+++ src/sphinx/2-mysql_demo_data.sh
@@ -5,8 +5,11 @@ wget "https://raw.github.com/alombarte/utilities/master/sql/spain_comunidades_au
 wget "https://raw.github.com/alombarte/utilities/master/sql/spain_municipios_INE.sql" --no-check-certificate
 wget "https://raw.github.com/alombarte/utilities/master/sql/spain_provincias.sql" --no-check-certificate
 
-echo "CREATE database sphinx_demo;\nCREATE USER 'sphinx'@'127.0.0.1' IDENTIFIED BY 'sspphhiinnxx001122';\nGRANT SELECT ON *.* TO 'sphinx'@'127.0.0.1';\n" > $FILE
+echo "DROP DATABASE IF EXISTS sphinx_demo; \
+CREATE database sphinx_demo; \
+GRANT SELECT ON sphinx_demo.* to 'sphinx'@'127.0.0.1' identified by 'sspphhiinnxx001122'; \
+USE sphinx_demo;" > $FILE
 cat spain_* >> $FILE
 
-mysql -u root -p sphinx_demo < $FILE
+mysql -u root -p < $FILE
 rm $FILE spain_*
