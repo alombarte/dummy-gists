54ac781b88fc36898a1acae722330fca17546701
Albert Lombarte <alombarte@gmail.com>
2014-03-04 00:22:33 +0100

    Added filtering and pagination

--- src/sphinx/search_example.php
+++ src/sphinx/search_example.php
@@ -13,11 +13,18 @@ $sphinx = new \SphinxClient();
 $sphinx->SetServer( '127.0.0.1', 9312);
 $sphinx->SetMatchMode( SPH_MATCH_EXTENDED );
 $sphinx->SetSortMode( SPH_SORT_RELEVANCE );
+
+// Restrict to Barcelona and Girona (id_provincia=8, id_provincia=17):
+$sphinx->SetFilter( 'id_provincia', array( 8, 17 ) );
+// Returns the first 5 results, let's say we want the "page 1".
+$sphinx->SetLimits( 0, 5 );
+
 $sphinx->AddQuery( $query_string, $index );
 $results = $sphinx->RunQueries();
 if ( !empty( $results[0]['matches'] ) )
 {
     var_dump( $results[0]['matches'] );
+    echo "Total matches: " . $results[0]['total_found'];
 }
 else
 {
